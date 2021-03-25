9650f0e152315aa00019d6b611ae44c140790c8e
i-mmanuel <emmanuel.jnr@outlook.com>
2021-03-25 22:02:47 +0000

    added email confirmation

--- models/User.js
+++ models/User.js
@@ -2,75 +2,79 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
-    first_name: {
-        type: String,
-        required: true,
-    },
-    last_name: {
-        type: String,
-        required: true,
-    },
-    phoneNumber: {
-        type: String,
-        unique: true,
-    },
-    date_of_birth: {
-        type: String,
-    },
-    email: {
-        type: String,
-        unique: true,
-        required: true,
-    },
-    password: {
-        type: String,
-        required: true,
-    },
-    membership_type: {
-        type: String,
-        default: 'new memeber',
-    },
+	first_name: {
+		type: String,
+		required: true,
+	},
+	last_name: {
+		type: String,
+		required: true,
+	},
+	phoneNumber: {
+		type: String,
+		unique: true,
+	},
+	date_of_birth: {
+		type: String,
+	},
+	email: {
+		type: String,
+		unique: true,
+		required: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	membership_type: {
+		type: String,
+		default: 'new memeber',
+	},
+	email_confimed: {
+		type: Boolean,
+		default: false,
+	},
 });
 
 userSchema.pre('save', function (next) {
-    const user = this;
+	const user = this;
 
-    if (!user.isModified('password')) {
-        return next();
-    }
+	if (!user.isModified('password')) {
+		return next();
+	}
 
-    bcrypt.genSalt(10, (error, salt) => {
-        if (error) {
-            return next(error);
-        }
+	bcrypt.genSalt(10, (error, salt) => {
+		if (error) {
+			return next(error);
+		}
 
-        bcrypt.hash(user.password, salt, (error, hash) => {
-            if (error) {
-                return next(error);
-            }
+		bcrypt.hash(user.password, salt, (error, hash) => {
+			if (error) {
+				return next(error);
+			}
 
-            user.password = hash;
-            next();
-        });
-    });
+			user.password = hash;
+			next();
+		});
+	});
 });
 
 userSchema.methods.comparePassword = function (candidatePassword) {
-    const user = this;
+	const user = this;
 
-    return new Promise((resolve, reject) => {
-        bcrypt.compare(candidatePassword, user.password, (error, isMatch) => {
-            if (error) {
-                return reject(error);
-            }
+	return new Promise((resolve, reject) => {
+		bcrypt.compare(candidatePassword, user.password, (error, isMatch) => {
+			if (error) {
+				return reject(error);
+			}
 
-            if (!isMatch) {
-                return reject(false);
-            }
+			if (!isMatch) {
+				return reject(false);
+			}
 
-            resolve(true);
-        });
-    });
+			resolve(true);
+		});
+	});
 };
 
 mongoose.model('User', userSchema);
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,65 +1,82 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
+const { mailer } = require('../config/mailer');
 const User = mongoose.model('User');
 
 const router = express.Router();
 
 router.post('/signup', async (request, response) => {
-    const {
-        email,
-        password,
-        first_name,
-        last_name,
-        phoneNumber,
-        date_of_birth,
-    } = request.body;
+	const {
+		email,
+		password,
+		first_name,
+		last_name,
+		phoneNumber,
+		date_of_birth,
+	} = request.body;
 
-    try {
-        const user = new User({
-            email,
-            password,
-            first_name,
-            last_name,
-            phoneNumber,
-            date_of_birth,
-        });
-        await user.save();
+	try {
+		const user = new User({
+			email,
+			password,
+			first_name,
+			last_name,
+			phoneNumber,
+			date_of_birth,
+		});
+		await user.save();
 
-        const token = jwt.sign({ userId: user._id }, 'This_is_a_secret_key!');
-        response.send({ token, user });
-    } catch (error) {
-        return response.status(422).send({ error: error });
-    }
+		const token = jwt.sign({ userId: user._id }, 'This_is_a_secret_key!');
+		response.send({ token, user });
+
+		const emailBody = `<h1>Hello ${first_name}.
+            <p>Here is to help you confirm your email address:</p>
+            <br/>
+            <h2>Click on this to confirm your email: <a href="https://fl-app-v1.herokuapp.com/confirmation/${user._id}">CLICK THIS TO CONFIRM YOUR EMAIL</a>
+            <br />
+            <p>Contact me for any questions</p>`;
+		mailer(first_name, email, emailBody);
+	} catch (error) {
+		return response.status(422).send({ error });
+	}
 });
 
 router.post('/signin', async (request, response) => {
-    const { email, password } = request.body;
+	const { email, password } = request.body;
 
-    if (!email || !password) {
-        return response
-            .status(422)
-            .send({ error: 'Please provide a valid email and password' });
-    }
+	if (!email || !password) {
+		return response
+			.status(422)
+			.send({ error: 'Please provide a valid email and password' });
+	}
 
-    const user = await User.findOne({ email });
+	const user = await User.findOne({ email });
 
-    if (!user) {
-        return response
-            .status(422)
-            .send({ error: 'Invalid  password or email' });
-    }
+	if (!user) {
+		return response.status(422).send({ error: 'Invalid  password or email' });
+	}
 
-    try {
-        await user.comparePassword(password);
+	try {
+		await user.comparePassword(password);
 
-        const token = jwt.sign({ userId: user._id }, 'This_is_a_secret_key!');
-        response.send({ token, user });
-    } catch (error) {
-        return response
-            .status(422)
-            .send({ error: 'Invalid password or email' });
-    }
+		const token = jwt.sign({ userId: user._id }, 'This_is_a_secret_key!');
+		response.send({ token, user });
+	} catch (error) {
+		return response.status(422).send({ error: 'Invalid password or email' });
+	}
+});
+
+router.post('/confimation/:id', async (request, response) => {
+	const id = request.params.id;
+
+	try {
+		const user = await User.findByIdAndUpdate(id, { email_confirmed: true });
+
+		response.sendFile('views/confimation.html');
+	} catch (error) {
+		return response.send({ error: 'There was no user' });
+	}
 });
 
 module.exports = router;
